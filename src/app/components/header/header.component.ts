590fac424f2784b07ff8f4e6478fe6e6e0dbbbcf
Bestin John <bestinjohn1993@gmail.com>
2024-12-05 16:49:08 +0100

    implement routing for product list and cart components, update header links

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { CartService } from './../../services/cart.service';
 import { Component, inject } from '@angular/core';
 import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [PrimaryButtonComponent],
+  imports: [PrimaryButtonComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
