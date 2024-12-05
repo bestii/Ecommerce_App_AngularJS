b566c2139a8dd690fb8fc69b6d3adb49d3967e03
Bestin John <bestinjohn1993@gmail.com>
2024-12-05 17:16:28 +0100

    add button component and update cart-item layout with button integration

--- src/app/pages/cart/cart-item/cart-item.component.ts
+++ src/app/pages/cart/cart-item/cart-item.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ButtonComponent } from '../../../components/button/button.component';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-cart-item',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './cart-item.component.html',
-  styleUrl: './cart-item.component.scss'
+  styleUrl: './cart-item.component.scss',
 })
 export class CartItemComponent {
-
+  cartService = inject(CartService);
 }
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,9 +1,11 @@
 import { CartService } from './../../services/cart.service';
 import { Component, inject } from '@angular/core';
+import { CartItemComponent } from './cart-item/cart-item.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CartItemComponent, OrderSummaryComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
